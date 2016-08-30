86d3d11dfb9451347af9e98e2868d00b78dd3b2d
innonatsu <thrine.legodi@gmail.com>
2016-08-31 00:46:59 +0200

    added new python script

---
+++ noga/simple.py
@@ -0,0 +1,8 @@
+import threading
+import socket
+
+
+print "================================================="
+print "\t\tWelcome to Folktales\n\twhere networks becomes your powers."
+print "================================================="
+
